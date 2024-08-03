f1241b9c5f06235242f61dac6e1532374abc3587
mhmdtfaily <mhmdalitfaily@gmail.com>
2024-08-03 23:41:12 +0300

    fix the migration

--- src/db/migrations/20240221160527_create_tables.js
+++ src/db/migrations/20240221160527_create_tables.js
@@ -5,6 +5,7 @@
 exports.up = async function (knex) {
   await knex.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
 
+  // Create user table
   await knex.schema.createTable('user', table => {
     table.uuid('id').unique().notNullable().primary().defaultTo(knex.raw('uuid_generate_v4()'));
     table.string('full_name');
@@ -13,18 +14,13 @@ exports.up = async function (knex) {
     table.timestamps(true, true);
   });
 
+  // Create chats table
   await knex.schema.createTable('chats', table => {
     table.uuid('id').unique().notNullable().primary().defaultTo(knex.raw('uuid_generate_v4()'));
     table.timestamps(true, true);
   });
 
-  await knex.schema.createTable('user_chats', table => {
-    table.uuid('id').unique().notNullable().primary().defaultTo(knex.raw('uuid_generate_v4()'));
-    table.uuid('user_id').unsigned().index().references('id').inTable('user').onDelete('CASCADE');
-    table.uuid('chat_id').unsigned().index().references('id').inTable('chats').onDelete('CASCADE');
-    table.timestamps(true, true);
-  });
-
+  // Create messages table
   await knex.schema.createTable('messages', table => {
     table.uuid('id').unique().notNullable().primary().defaultTo(knex.raw('uuid_generate_v4()'));
     table.uuid('user_id').unsigned().index().references('id').inTable('user').onDelete('CASCADE');
@@ -35,6 +31,19 @@ exports.up = async function (knex) {
     table.timestamp('read_at').nullable();
     table.timestamps(true, true);
   });
+
+  // Create user_chats table
+  await knex.schema.createTable('user_chats', table => {
+    table.uuid('id').unique().notNullable().primary().defaultTo(knex.raw('uuid_generate_v4()'));
+    table.uuid('user_id').unsigned().index().references('id').inTable('user').onDelete('CASCADE');
+    table.uuid('chat_id').unsigned().index().references('id').inTable('chats').onDelete('CASCADE');
+    table.timestamps(true, true);
+  });
+
+  // Add message_id foreign key to chats table
+  await knex.schema.table('chats', table => {
+    table.uuid('message_id').unsigned().index().references('id').inTable('messages').onDelete('CASCADE');
+  });
 };
 
 /**
@@ -42,8 +51,8 @@ exports.up = async function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = async function (knex) {
-  await knex.schema.dropTableIfExists('messages');
   await knex.schema.dropTableIfExists('user_chats');
+  await knex.schema.dropTableIfExists('messages');
   await knex.schema.dropTableIfExists('chats');
   await knex.schema.dropTableIfExists('user');
 };
